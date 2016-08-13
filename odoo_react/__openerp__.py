85639c0340cb172a7bcec948a01ec45a8e0d01aa
oalca <oscar@vauxoo.com>
2016-08-13 00:42:15 -0500

    [INI] Basic inlclude for react library on odoo env

---
+++ odoo_react/__openerp__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+# Copyright 2016 Vauxoo
+# Twitter: @oscarolar
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "React Odoo",
+    'summary': """
+        Includes React necessary libraries to compile JSX.
+        """,
+    'author': "Vauxoo",
+    'website': "http://vauxoo.com",
+    'category': 'Website',
+    'version': '9.0.0.1.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'website',
+    ],
+    'demo': [
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+}
